600b0492a0e840a0ed0bf16a778dffde6711cdaa
Lori Boyd <lori.p.boyd@gmail.com>
2020-02-17 03:41:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+
+let welcomeMessage = 'Welcome to Flatbook, ';
+
+welcomeMessage += currentUser + '!';
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)} !`
+
+
